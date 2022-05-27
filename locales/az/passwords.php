11ab6870e5d6037763f3f5c878c997c482571e42
Maharramoff <9804406+Maharramoff@users.noreply.github.com>
2022-05-27 23:04:58 +0400

    passwords(az): add missing translations and correct some

--- locales/az/passwords.php
+++ locales/az/passwords.php
@@ -12,9 +12,9 @@
 */
 
 return [
-    'reset'     => 'Şifrə yeniləndi',
-    'sent'      => 'Şifrə yeniləmə adresi sizə email olaraq göndərildi',
-    'throttled' => 'Yenidən cəhd əvvəl gözləyin edin.',
-    'token'     => 'Bu şifrə yeniləmə kodu yanlışdır',
-    'user'      => 'Bu email\'ə uyğun istifadəçi tapılmadı',
+    'reset'     => 'Şifrəniz sıfırlandı!',
+    'sent'      => 'Şifrə sıfırlama linkinizi e-poçtla göndərdik!',
+    'throttled' => 'Lütfən, yenidən cəhd etməzdən əvvəl gözləyin.',
+    'token'     => 'Şifrə sıfırlama kodu yanlışdır.',
+    'user'      => 'Göstərilən e-poçt ünvanına aid istifadəçi tapılmadı.',
 ];
